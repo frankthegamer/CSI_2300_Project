0ff21d0edfe9cde6b9c7d4821b055a7a07b54225
Frank <franksoftwareideas@gmail.com>
2025-04-14 22:01:35 -0400

    Add files via upload

---
+++ src/organizer/rule/Rule.java
@@ -0,0 +1,30 @@
+package organizer.rule;
+
+import java.nio.file.Path;
+import org.json.JSONObject;
+
+public interface Rule {
+
+    boolean matches(Path file);  // check if the rule matches a file
+    boolean equals(Object obj);  // check if two rules are equal
+    int hashCode();             // generate a hash code for the rule
+
+    JSONObject toJSON();
+    static Rule fromJSON(JSONObject json){
+        String type = json.getString("type");
+        return switch (type){
+            case "FileCategoryRule" -> new FileCategoryRule(json);
+            case "FileExtensionRule" -> new FileExtensionRule(json);
+            case "LastModifiedRule" -> new LastModifiedRule(json);
+            case "NameHasRule" -> new NameHasRule(json);
+            case "StringContainedRule" -> new StringContainedRule(json);
+            default -> throw new IllegalArgumentException("Unknown rule type: " + type);
+        };
+    }
+
+    @Override
+    String toString();
+}
+
+
+
